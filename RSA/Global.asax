ac325e51e9f98b2d37be2ba9820b381d90c6b6b4
RAHUL KUMAR <rahulinbscit@live.com>
2018-11-12 21:46:18 +0530

    Add project files.

---
+++ RSA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RSA.MvcApplication" Language="C#" %>
---
+++ RSA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RSA.Startup))]
+namespace RSA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
