66c38eeaaf8daa8d8f08a2c6dacaeda0dd392f98
RAHUL KUMAR <rahulinbscit@live.com>
2018-12-28 02:01:48 +0530

    RSA Work for Web API

--- RSAWebApplication/Controllers/T_News_MastersController.cs
+++ RSAWebApplication/Controllers/T_News_MastersController.cs
@@ -10,44 +10,49 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using RSAWebApplication.DBModels;
+using RSAWebApplication.Models;
 
 namespace RSAWebApplication.Controllers
 {
     public class T_News_MastersController : ApiController
     {
         private RSADBEntities db = new RSADBEntities();
-        private IList<T_News_Masters> ObjNewsList = new List<T_News_Masters>();
+        private IList<NewsModel> ObjNewsList = new List<NewsModel>();
         // GET: api/T_News_Masters
         public IHttpActionResult GetT_News_Masters()
         {
             var Result = db.T_News_Masters;
             foreach (var News in Result)
             {
-                ObjNewsList.Add(new T_News_Masters()
+                ObjNewsList.Add(new NewsModel()
                 {
                     NewsID = News.NewsID,
                     Active = News.Active,
                     CreatedBy = News.CreatedBy,
-                    CreatedDate = News.CreatedDate,
+                    CreatedDate = News.CreatedDate.ToString("dd-MMM-yyyy"),
                     Img1 = News.Img1,
-                    Img2 = News.Img2,
-                    Img3 = News.Img3,
-                    Img4 = News.Img4,
-                    Img5 = News.Img5,
+                    //Img2 = News.Img2,
+                    //Img3 = News.Img3,
+                    //Img4 = News.Img4,
+                    //Img5 = News.Img5,
                     Location = News.Location,
                     ModifiedBy = News.ModifiedBy,
-                    ModifiedDate = News.ModifiedDate,
+                    ModifiedDate = News.ModifiedDate.ToString("dd-MMM-yyyy"),
                     NewsHeading = News.NewsHeading,
                     NewsSubCategoryID = News.NewsSubCategoryID,
-                    NewsType = News.NewsType,
+                    NewsTypeID = News.NewsType,
                     NoOfNewsViews = News.NoOfNewsViews,
-                    Para1 = News.Para1,
-                    Para2 = News.Para2,
-                    Para3 = News.Para3,
-                    Para4 = News.Para4,
-                    Para5 = News.Para5,
-                    Remarks = News.Remarks,
-                    SubHeading = News.SubHeading
+                    //Para1 = News.Para1,
+                    //Para2 = News.Para2,
+                    //Para3 = News.Para3,
+                    //Para4 = News.Para4,
+                    //Para5 = News.Para5,
+                    //Remarks = News.Remarks,
+                    SubHeading = News.SubHeading,
+                    NewsCategoryID = News.M_NewsSubCategoryMasters.NewsCategory,
+                    NewsCategory = News.M_NewsSubCategoryMasters.M_NewsCategoryMaster.CategoryName,
+                    NewsSubCategory = News.M_NewsSubCategoryMasters.SubCategory,
+                    NewsType = News.M_RSAMasters.TableValue
                 });
             }
             return Ok(ObjNewsList);
@@ -63,7 +68,37 @@ namespace RSAWebApplication.Controllers
             {
                 return NotFound();
             }
-            return Ok(t_News_Masters);
+            NewsModel ObjNewsDetails = new NewsModel()
+            {
+                NewsID =  t_News_Masters.NewsID,
+                Active =  t_News_Masters.Active,
+                CreatedBy =  t_News_Masters.CreatedBy,
+                CreatedDate =  t_News_Masters.CreatedDate.ToString("dd-MMM-yyyy"),
+                Img1 =  t_News_Masters.Img1,
+                Img2 = t_News_Masters.Img2,
+                Img3 = t_News_Masters.Img3,
+                Img4 = t_News_Masters.Img4,
+                Img5 = t_News_Masters.Img5,
+                Location =  t_News_Masters.Location,
+                ModifiedBy =  t_News_Masters.ModifiedBy,
+                ModifiedDate =  t_News_Masters.ModifiedDate.ToString("dd-MMM-yyyy"),
+                NewsHeading =  t_News_Masters.NewsHeading,
+                NewsSubCategoryID =  t_News_Masters.NewsSubCategoryID,
+                NewsTypeID =  t_News_Masters.NewsType,
+                NoOfNewsViews =  t_News_Masters.NoOfNewsViews,
+                Para1 = t_News_Masters.Para1,
+                Para2 = t_News_Masters.Para2,
+                Para3 = t_News_Masters.Para3,
+                Para4 = t_News_Masters.Para4,
+                Para5 = t_News_Masters.Para5,
+                Remarks = t_News_Masters.Remarks,
+                SubHeading =  t_News_Masters.SubHeading,
+                NewsCategoryID =  t_News_Masters.M_NewsSubCategoryMasters.NewsCategory,
+                NewsCategory =  t_News_Masters.M_NewsSubCategoryMasters.M_NewsCategoryMaster.CategoryName,
+                NewsSubCategory =  t_News_Masters.M_NewsSubCategoryMasters.SubCategory,
+                NewsType =  t_News_Masters.M_RSAMasters.TableValue
+            };
+            return Ok(ObjNewsDetails);
         }
 
         // PUT: api/T_News_Masters/5
---
+++ RSAWebApplication/Models/NewsModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RSAWebApplication.Models
+{
+    public class NewsModel
+    {
+        public int NewsID { get; set; }
+        public int NewsTypeID { get; set; }
+        public string NewsType { get; set; }
+        public int NewsCategoryID { get; set; }
+        public string NewsCategory { get; set; }
+        public int NewsSubCategoryID { get; set; }
+        public string NewsSubCategory { get; set; }
+        public string NewsHeading { get; set; }
+        public string SubHeading { get; set; }
+        public string Para1 { get; set; }
+        public string Para2 { get; set; }
+        public string Para3 { get; set; }
+        public string Para4 { get; set; }
+        public string Para5 { get; set; }
+        public string Img1 { get; set; }
+        public string Img2 { get; set; }
+        public string Img3 { get; set; }
+        public string Img4 { get; set; }
+        public string Img5 { get; set; }
+        public string Remarks { get; set; }
+        public string Location { get; set; }
+        public int NoOfNewsViews { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public string ModifiedDate { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
